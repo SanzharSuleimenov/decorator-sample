1de5a9455ad84af9668d07bc366bec18db43468e
Sanzhar Suleimenov <sanzhar.suleimenov@gmail.com>
2022-11-23 00:55:19 +0600

    FileDataSource base draft logic

--- src/io/rundev/refactoring_guru/FileDataSource.java
+++ src/io/rundev/refactoring_guru/FileDataSource.java
@@ -1,5 +1,7 @@
 package io.rundev.refactoring_guru;
 
+import java.io.*;
+
 public class FileDataSource implements DataSource {
 
   private final String fileName;
@@ -10,13 +12,26 @@ public class FileDataSource implements DataSource {
 
   @Override
   public void writeData(String data) {
-    System.out.printf("Writing data: '%s' to %s", data, fileName);
-    // TODO: implement writing to file
+    System.out.printf("Writing data: '%s' to %s\n", data, fileName);
+    File file = new File(fileName);
+    try (OutputStream out = new FileOutputStream(file)) {
+      out.write(data.getBytes(), 0, data.length());
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
   }
 
   @Override
   public String readData() {
-    // TODO implement reading from file
-    return null;
+    System.out.printf("Reading data from '%s'\n", fileName);
+    char[] buffer = null;
+    File file = new File(fileName);
+    try (FileReader reader = new FileReader(file)) {
+      buffer = new char[(int) file.length()];
+      reader.read(buffer);
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+    return new String(buffer);
   }
 }
