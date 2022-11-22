de89e42b7f4ffc7353f1b7e0424b6354cf4f3914
Sanzhar Suleimenov <sanzhar.suleimenov@gmail.com>
2022-11-23 00:59:24 +0600

    Decorator implementation

--- src/io/rundev/refactoring_guru/decorator/EncryptionDecorator.java
+++ src/io/rundev/refactoring_guru/decorator/EncryptionDecorator.java
@@ -2,6 +2,8 @@ package io.rundev.refactoring_guru.decorator;
 
 import io.rundev.refactoring_guru.DataSource;
 
+import java.util.Base64;
+
 public class EncryptionDecorator extends DataSourceDecorator {
 
   public EncryptionDecorator(DataSource wrappee) {
@@ -16,5 +18,21 @@ public class EncryptionDecorator extends DataSourceDecorator {
   @Override
   public String readData() {
     return super.readData();
+  }
+
+  private String encode(String data) {
+    byte[] result = data.getBytes();
+    for (int i = 0; i < result.length; i++) {
+      result[i] += (byte) 1;
+    }
+    return Base64.getEncoder().encodeToString(result);
+  }
+
+  private String decode(String data) {
+    byte[] result = data.getBytes();
+    for (int i = 0; i < result.length; i++) {
+      result[i] -= (byte) 1;
+    }
+    return new String(result);
   }
 }
